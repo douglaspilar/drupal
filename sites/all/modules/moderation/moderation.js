fc2636a00dea7a52a31ea6d2a7aa40a3bdafa0a1
maiquelleonel <skywishmtfk@gmail.com>
2010-09-28 19:15:29 -0300

    corrigido ajax da moderacao de conteudo

--- sites/all/modules/moderation/moderation.js
+++ sites/all/modules/moderation/moderation.js
@@ -4,10 +4,11 @@ Drupal.moderationPreview = function() {
   var id = $(this).attr("id");
   var obj_id = $(this).attr("id").split('-')[2];
   $(this).next('.moderation-info').after('<div id="moderation-preview-'+obj_id+'" class="moderation-preview"></div>');
-  
+
   $('.moderation-title > a', this).click(function () {
 	var preview = $('#moderation-preview-'+obj_id);
-    var url = window.location.protocol+'//'+window.location.hostname+$(this).attr("href");
+    //var url = window.location.protocol+'//'+window.location.hostname+$(this).attr("href");
+    var url = $(this).attr('href');
     if (preview.html().length == 0) {
       //load node data
       var html = $.ajax({
@@ -22,7 +23,8 @@ Drupal.moderationPreview = function() {
     preview.toggle();
 
     $.ajax({
-      url: window.location.protocol+'//'+window.location.hostname+Drupal.settings.basePath+"?q=moderation/"+Drupal.settings.moderationType+"/"+obj_id+"/get/moderate&js=1",
+      //url: window.location.protocol+'//'+window.location.hostname+Drupal.settings.basePath+"?q=moderation/"+Drupal.settings.moderationType+"/"+obj_id+"/get/moderate&js=1",
+      url: Drupal.settings.basePath+"?q=moderation/"+Drupal.settings.moderationType+"/"+obj_id+"/get/moderate&js=1",
       dataType: "json",
       success: function(status) {
         $('#moderation-preview-'+obj_id+' .moderation-messages').remove();
@@ -31,7 +33,7 @@ Drupal.moderationPreview = function() {
         }
       }
     });
-    
+
     return false;
   });
 }
@@ -40,14 +42,15 @@ Drupal.moderationButton = function() {
   var id = $(this).attr("id");
   var type = id.split('-')[1]
   var obj_id = id.split('-')[3];
-  var url = window.location.protocol+'//'+window.location.hostname+$(this).attr("href");
+  //var url = window.location.protocol+'//'+window.location.hostname+$(this).attr("href");
+  var url = $(this).attr("href");
   var text = {
     'status': {0: Drupal.t('publish'), 1: Drupal.t('unpublish')},
     'promote': {0: Drupal.t('promote'), 1: Drupal.t('demote')},
     'sticky': {0: Drupal.t('make sticky'), 1: Drupal.t('remove stickiness')},
     'moderate': {0: Drupal.t('moderate'), 1: Drupal.t('unmoderate')}
   };
-  
+
   $(this).click(function () {
     //load node data
     var html = $.ajax({
@@ -81,3 +84,4 @@ if (Drupal.jsEnabled) {
     $('.moderation-moderate-link').each(Drupal.moderationButton);
   });
 }
+
